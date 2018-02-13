0295b671ffa3fc1797c75e6b252c170451868c13
Garystdio <1954758323@qq.com>
2018-02-13 16:11:14 +0800

    Create Unlimited_Types.hpp

---
+++ Unlimited_Types.hpp
@@ -0,0 +1,13 @@
+#incldue <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <windows.h>
+using namespace std;
+
+namespace Unlimited{
+  class unimited_integer{
+    public:
+      
+  };
+};
